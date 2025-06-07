53fea7b0d539bbe5e51cd517efca8a937a1315d2
kerrakir <kerrakir@gmail.com>
2025-06-07 14:42:36 +0300

    Update matrix.go

--- matrix.go
+++ matrix.go
@@ -39,30 +39,25 @@ func clearChar(x, y int) {
 
 func rainColumn(x int) {
 	for {
-		// новая "линия"
 		length := rand.Intn(maxLen-minLen) + minLen
-		startY := rand.Intn(height/2) * -1 // начинать немного выше экрана
+		startY := rand.Intn(height/2) * -1 
 		speed := time.Duration(rand.Intn(20)+20) * time.Millisecond
 
 		for y := startY; y < height+length; y++ {
-			// очистка головы прошлого цикла
 			if y-length >= 0 {
 				clearChar(x, y-length)
 			}
 
-			// хвост
 			for i := 1; i < length; i++ {
 				yy := y - i
-				drawChar(x, yy, charset[rand.Intn(len(charset))], "\033[2;32m") // тускло-зелёный
+				drawChar(x, yy, charset[rand.Intn(len(charset))], "\033[2;32m") 
 			}
 
-			// голова
-			drawChar(x, y, charset[rand.Intn(len(charset))], "\033[1;37m") // ярко-белый
+			drawChar(x, y, charset[rand.Intn(len(charset))], "\033[1;37m") 
 
 			time.Sleep(speed)
 		}
 
-		// пауза перед следующей линией
 		time.Sleep(time.Duration(rand.Intn(1000)+200) * time.Millisecond)
 	}
 }
@@ -70,14 +65,13 @@ func rainColumn(x int) {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	clearScreen()
-	fmt.Print("\033[?25l")       // скрыть курсор
-	defer fmt.Print("\033[?25h") // показать при выходе
+	fmt.Print("\033[?25l")       
+	defer fmt.Print("\033[?25h")
 
-	// каждый столбец — отдельная горутина
 	for x := 1; x < width; x += 2 {
 		go rainColumn(x)
-		time.Sleep(10 * time.Millisecond) // для асинхронности старта
+		time.Sleep(10 * time.Millisecond)
 	}
 
-	select {} // блокируем основной поток
+	select {} 
 }
